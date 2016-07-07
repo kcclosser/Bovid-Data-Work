79c7c5b111e0cf170f2a3a4af8e20edb0f8706e8
Kenny Closser <kcclosser@berkeley.edu>
2016-07-06 17:10:16 -0700

    fixed first task, new data for average of averages consolidated

--- ConsolEcologicalData.R
+++ ConsolEcologicalData.R
@@ -2,10 +2,15 @@
 setwd("/Users/kennyclosser/Downloads")
 df <- read.csv(file="Ungulata_Alt_MAP_MAT.csv", header=FALSE, stringsAsFactors=FALSE,
                   fileEncoding="latin1", skip=16)
-df$total_area <- aggregate(df$V25, by=list(df$V1), "sum") #not working, need to find a way to expand sum vector
-df$factors <- transform(df, new =  V25/ total_area)
-df$product <- df$factors * df$V31
-df$V26 <- aggregate(df$product, by=list(df$V1), "sum") #same issue as line 4
+newdf <- data.frame(aggregate(df$V25, by=list(IDnum = df$V1, V2=df$V2, df$V1), "sum")) #not working, need to find a way to expand sum vector
+ndf <- merge(df, newdf, by="V2",all=TRUE)
+ndf[, "div"] <- ndf[, "V31"] / ndf[, "x"]
+sapply(ndf, class)
+ndf$product <- ndf$div * ndf$V31
+tempfile <- data.frame(aggregate(ndf$product, by=list(V2=ndf$V2, ndf$V1), "sum")) #same issue as line 4
+finalfile <- merge(ndf, tempfile, by="V2", all=TRUE)
+finalfile <- finalfile[ , -which(names(finalfile) %in% c("div","product","Group.2"))]
+colnames(finalfile)[49] <- "alt_mean"
 
 #TASK2
 aggregate(df$V33,by=list(df$V1), function(x) (min(x)) ) #same as above, can easily replicate for selection of other columns after debugged
