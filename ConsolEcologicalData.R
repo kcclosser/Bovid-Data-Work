3c5bf1f8bbd4ab0ab469d210d002e3f5ffd1e450
Kenny Closser <kcclosser@berkeley.edu>
2016-07-11 10:46:01 -0700

    Fixed tasks 2 and 3, changed mean in task 1

--- ConsolEcologicalData.R
+++ ConsolEcologicalData.R
@@ -2,21 +2,24 @@
 setwd("/Users/kennyclosser/Downloads")
 df <- read.csv(file="Ungulata_Alt_MAP_MAT.csv", header=FALSE, stringsAsFactors=FALSE,
                   fileEncoding="latin1", skip=16)
-newdf <- data.frame(aggregate(df$V25, by=list(IDnum = df$V1, V2=df$V2, df$V1), "sum")) #not working, need to find a way to expand sum vector
+newdf <- data.frame(aggregate(df$V25, by=list(IDnum = df$V1, V2=df$V2, df$V1), "sum")) 
 ndf <- merge(df, newdf, by="V2",all=TRUE)
-ndf[, "div"] <- ndf[, "V31"] / ndf[, "x"]
-sapply(ndf, class)
-ndf$product <- ndf$div * ndf$V31
-tempfile <- data.frame(aggregate(ndf$product, by=list(V2=ndf$V2, ndf$V1), "sum")) #same issue as line 4
+ndf[, "div"] <- ndf[, "V36"] / ndf[, "x"]
+ndf$product <- ndf$div * ndf$V36
+tempfile <- data.frame(aggregate(ndf$product, by=list(V2=ndf$V2, ndf$V1), "sum"))
 finalfile <- merge(ndf, tempfile, by="V2", all=TRUE)
 finalfile <- finalfile[ , -which(names(finalfile) %in% c("div","product","Group.2"))]
-colnames(finalfile)[49] <- "alt_mean"
+colnames(finalfile)[49] <- "temp_aggregate" #unsure which mean to use here (MAP or MAT) but the structure is correct
+View(finalfile) #Shows a new file with the same data as the file read in and an additional column at the end with the desired overall mean
 
 #TASK2
-aggregate(df$V33,by=list(df$V1), function(x) (min(x)) ) #same as above, can easily replicate for selection of other columns after debugged
-#for species min/max, do the same thing as above. Figure out how to replace column too
+newdf <- data.frame(aggregate(df$V33, by=list(IDnum = df$V1, V2=df$V2), "min" ))
+ndf <- merge(df, newdf, by="V2",all=TRUE)
+View(ndf) #final column contains selected minimum and maximum, easily repeated
 
 #TASK3
 df$squares_alt <- df$V32^2
-df$sum_variances_alt <- aggregate(df$squares_alt,by=list(df$V1), "sum")#same issue as above
-df$sum_stddev_alt <- df$sum_variances_alt^1/2
+newdf <- data.frame(aggregate(df$squares_alt, by=list(IDnum = df$V1, V2 = df$V2), "sum"))
+ndf <- merge(df, newdf, by="V2", all=TRUE)
+ndf$sum_stddev_alt <- ndf$x^(1/2)
+View(ndf) #last column is a sum of variances square rooted, so a standard deviation for the entire species
